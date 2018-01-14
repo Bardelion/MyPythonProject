68b28d3d4ebb83cef4456ca0e0abec16b6263743
Bardelion <34720459+Bardelion@users.noreply.github.com>
2018-01-14 10:30:06 +0200

    code1.py

--- code1.py
+++ code1.py
@@ -1,1 +1,8 @@
-print ('hello world')
+import tkinter
+from tkinter import *
+
+a = Tk()
+a.title("This is a Tkinter in python example")
+a.geometry("500x500+100+50")
+ThisIsLabel1 = Label(text="This is a Label",fg='red',bg='yellow',font=20).pack()
+
